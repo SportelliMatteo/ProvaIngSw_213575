a4522786f20662b0fc8ecd92306083528b64f8ff
SportelliMatteo <sportelli.matteo1@gmail.com>
2022-10-28 15:38:15 +0200

    Implementazione metodo StringToIntConvert

---
+++ src/test/java/sportelli/provaIngsw/FunnyAlgorithmsTest.java
@@ -0,0 +1,13 @@
+package sportelli.provaIngsw;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class FunnyAlgorithmsTest 
+{
+    
+}
